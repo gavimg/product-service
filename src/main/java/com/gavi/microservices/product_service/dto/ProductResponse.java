5943e5e7e281266ac094b701c9262c839d7e466c
gavimg <gavigadagi@gmail.com>
2026-03-26 00:11:16 +0530

    Initial Commit Product Micro Service

---
+++ src/main/java/com/gavi/microservices/product_service/dto/ProductResponse.java
@@ -0,0 +1,5 @@
+package com.gavi.microservices.product_service.dto;
+
+import java.math.BigDecimal;
+
+public record ProductResponse(String id, String name, String description, BigDecimal price) { }
